3672ae5671401a1301bca2c350a3a76937e45c07
Kevin Lopez <kslopez709@misena.edu.co>
2022-12-09 10:01:10 -0500

    v2

--- views/modules/usuarios/Usuarios.php
+++ views/modules/usuarios/Usuarios.php
@@ -493,6 +493,7 @@ VALUES('$cant', '$reg_nombre', '$reg_phone', '$reg_job', '$reg_id_type', '$reg_i
     </div>
 
     <script src="/script/checks.js"></script>
+    <script src="/script/reg.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
